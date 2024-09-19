434f4f117310c04561dc5e88ebe3d40244e43d11
RedoyRKS <redoyshaha06@gmail.com>
2024-09-19 20:02:19 +0600

    r

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TestComponent from './src/components/test-compontent';
 import CounterScren from './src/screens/counter-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-        {/* <TestComponent/> */}
-        <CounterScren/>
-    </View>
+    // <View style={styles.container}>
+    //     {/* <TestComponent/> */}
+    //     <CounterScren/>
+    // </View>
+    <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen name="Counter" component={CounterScren}/>
+        <stack.Screen name="Quiz-1" component={CounterScren}/>
+      </stack.Navigator>
+
+    </NavigationContainer>
     
   );
 }
